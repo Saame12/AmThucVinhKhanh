67887d570b7734c5b5c89b11faf90d43065b4028
WuyyHo <hobaohuy.05@gmail.com>
2026-04-15 19:51:15 +0700

    Add files via upload

---
+++ VinhKhanhFood.Admin/User.cs
@@ -0,0 +1,18 @@
+﻿namespace VinhKhanhFood.Admin.Models
+{
+    public class UsageHistory
+    {
+        public int Id { get; set; }
+
+        public int UserId { get; set; }
+        public string? UserName { get; set; }
+
+        public string? Action { get; set; }
+
+        public int PoiId { get; set; }
+        public string? PoiName { get; set; }
+
+        public DateTime CreatedAt { get; set; }
+        public string? Role { get; set; } // 🔥 NEW
+    }
+}
